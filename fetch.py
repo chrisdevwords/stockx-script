99a6df7fe5e91637ec689855898814a7152bc207
Edwards, Chris <Christopher.Edwards@nike.com>
2018-11-27 13:02:09 -0800

    module outputs a spreadsheet of sizes, sales, date

--- fetch.py
+++ fetch.py
@@ -2,15 +2,37 @@ from argparse import ArgumentParser
 import json
 
 from stockxsdk import Stockx
+import xlwt
 
-# not sure about this...
+
+# not sure about this, i think it's the right activity code
 ACTIVITY_TYPE_SOLD = "480"
+
+
+def write_size_report(file_name, sales):
+    book = xlwt.Workbook()
+    sizes = sorted({sale["shoeSize"] for sale in sales}, key=lambda x:float(x))
+    for size in sizes:
+        sheet = book.add_sheet(f"Size {size}")
+        sheet.write(0, 0, "DATE")
+        sheet.write(0, 3, "AMOUNT")
+
+        size_sales = sorted(
+            [sale for sale in sales if sale["shoeSize"] == size],
+            key=lambda x: x["createdAt"])
+
+        for i in range(0, len(size_sales)):
+            date = size_sales[i]["createdAt"]
+            sheet.write(i + 1, 0, date)
+            sheet.write(i + 1, 3, f"${size_sales[i]['amount']}")
+    book.save(file_name)
+    return file_name
 
 if __name__ == "__main__":
 
     # todo parse from args
     parser = ArgumentParser(
-        description="Take a style number and get the latest sales from Stock X.")
+        description="Take a style number and generate a spreadsheet of sales from Stock X.")
     parser.add_argument("style_id", help="The style id for the shoe.")
     parser.add_argument("--size", help="Optionally restrict output to a single size.")
     parser.add_argument("--email", help="Email Address for your StockX account.")
@@ -27,9 +49,13 @@ if __name__ == "__main__":
     sales = stx._Stockx__get_activity(product_id, ACTIVITY_TYPE_SOLD)
     if args.size:
         sales = [s for s in sales if s["shoeSize"] == args.size]
+
     print("Most recent sale: ${localAmount} on {createdAt}".format(**sales[0]))
     sales = sorted(sales, key=lambda x: x["amount"])
     print("Highest sale: ${localAmount} on {createdAt}".format(**sales[-1]))
     print("Lowest sale: ${localAmount} on {createdAt}".format(**sales[0]))
+    print("Writing spreadshet...")
+    file_name = write_size_report(f"{args.style_id}.xls", sales)
+    print(f"{file_name} saved.")
     #print(json.dumps(sales, indent=True))
     exit(0)
